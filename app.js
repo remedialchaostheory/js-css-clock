99e08a0f9ec75c492e19f29c5ccb0acde8984b38
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-07-12 13:55:26 -0700

    Remove full rotation of hands at every top of the minute

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 (function () {
   'use strict';
+
   const secondHand = document.querySelector('.second-hand');
   const minHand = document.querySelector('.min-hand');
   const hourHand = document.querySelector('.hour-hand');
@@ -8,7 +9,6 @@
     const now = new Date();
 
     const seconds = now.getSeconds();
-    // console.log(seconds);
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
@@ -19,6 +19,17 @@
     const hours = now.getHours();
     const hoursDegrees = ((hours / 12) * 360) + 90;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    // Removes full rotation of hands at every top of the minute
+    const allHands = document.querySelectorAll('.hand');
+    if (seconds === 0)  {
+      allHands.forEach(hand => hand.style.transition = 'none');
+    }
+    else {
+      allHands.forEach(hand => hand.style.transition = '');
+    }
+
+
 
   }
   setInterval(setDate, 1000);
